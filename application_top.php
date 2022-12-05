274f6232a6bb9f33fe637ff1b3e4aeb22fdb7d85
scatterlogical <scatterlogical@gmail.com>
2022-12-05 13:18:19 +1000

    enabled display_errors

--- application_top.php
+++ application_top.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 'On');
+
 require 'plugins/rukoxero/vendor/autoload.php';
 require 'plugins/rukoxero/classes/DBStorage.php';
 require 'plugins/rukoxero/classes/Authentication.php';
